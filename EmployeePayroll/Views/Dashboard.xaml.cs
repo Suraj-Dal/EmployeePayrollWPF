b408ae60c3df45f51d9a7f1e96f5a3e2838c36df
Suraj-Dal <dsurajdal10@gmail.com>
2023-01-01 13:28:51 +0530

    [Suraj]: Added Employee payroll desktop app developed using WPF c#.

---
+++ EmployeePayroll/Views/Dashboard.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeePayroll
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            LoadGrid();
+        }
+        SqlConnection connection = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=EmployeePayroll;Integrated Security=True");
+        public void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("select EmpID, Name,Profile, Gender, Dept, Salary, Date, Note from Employee", connection);
+            DataTable dataTable = new DataTable();
+            connection.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dataTable.Load(sdr);
+            connection.Close();
+            datagrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void AddUser(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void DeleteEvent(object sender, RoutedEventArgs e)
+        {
+            //SqlCommand sqlCommand = new SqlCommand("dbo.InsertIntoEmployee", connection);
+            //sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            //connection.Open();
+            //sqlCommand.Parameters.AddWithValue("@EmpID", id.GetValue);
+            DataRowView row = (DataRowView)datagrid.SelectedItem;
+            MessageBoxResult messageBoxResult = MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                SqlCommand sqlCommand = new SqlCommand("dbo.DeleteFromEmployeeTable", connection);
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                connection.Open();
+                sqlCommand.Parameters.AddWithValue("@EmpID", row["EmpID"]);
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+                LoadGrid();
+            }
+        }
+        private void EditEvent(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)datagrid.SelectedItem;
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.name_txt.Text = row["Name"].ToString();
+            mainWindow.genderMenu.Text = row["Gender"].ToString();
+            mainWindow.sliderValue.Text = row["Salary"].ToString();
+            string fullDate = row["Date"].ToString();
+            string[] date = fullDate.Split(' ');
+            mainWindow.date_combo.Text = date[0];
+            mainWindow.month_combo.Text = date[1];
+            mainWindow.year_combo.Text = date[2];
+            mainWindow.note_txt.Text = row["Note"].ToString();
+            mainWindow.isUpdate = true;
+            mainWindow.EmpID = Convert.ToInt32(row["EmpID"]);
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ EmployeePayroll/Views/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeePayroll
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public string profileLink = "";
+        public bool isUpdate = false;
+        public int EmpID;
+        SqlConnection connection = new SqlConnection(@"Data Source=localhost\SQLEXPRESS;Initial Catalog=EmployeePayroll;Integrated Security=True");
+        public void Clear()
+        {
+            name_txt.Clear();
+            note_txt.Clear();
+            a_radio.IsChecked = false;
+            b_radio.IsChecked = false;
+            c_radio.IsChecked = false;
+            d_radio.IsChecked =false;
+            genderMenu.Text = "";
+            hr_chk.IsChecked = false;
+            sales_chk.IsChecked = false;    
+            fin_chk.IsChecked = false;
+            eng_chk.IsChecked = false;
+            other_chk.IsChecked = false;
+            slValue.Value = 0;
+        }
+        public bool isValid()
+        {
+            if(name_txt.Text == String.Empty)
+            {
+                MessageBox.Show("Name required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (a_radio.IsChecked == false && b_radio.IsChecked == false && c_radio.IsChecked == false && d_radio.IsChecked == false)
+            {
+                MessageBox.Show("Profile image required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (hr_chk.IsChecked == false && sales_chk.IsChecked == false && fin_chk.IsChecked == false & eng_chk.IsChecked == false && other_chk.IsChecked == false)
+            {
+                MessageBox.Show("Department required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (genderMenu.Text == String.Empty)
+            {
+                MessageBox.Show("Gender required.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (slValue.Value == 0)
+            {
+                MessageBox.Show("Salary cannot be zero.", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+        private void Submit_Click(object sender, RoutedEventArgs e) 
+        {
+            if (!isUpdate)
+            {
+                if (isValid())
+                {
+                    string date = date_combo.Text + " " + month_combo.Text + " " + year_combo.Text;
+                    var selectedItems = listOption.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => x.Content).ToList();
+                    using (connection)
+                        try
+                        {
+                            SqlCommand sqlCommand = new SqlCommand("dbo.InsertIntoEmployee", connection);
+
+                            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                            connection.Open();
+
+                            sqlCommand.Parameters.AddWithValue("@Name", name_txt.Text);
+                            sqlCommand.Parameters.AddWithValue("@Profile", profileLink);
+                            sqlCommand.Parameters.AddWithValue("@Gender", genderMenu.Text);
+                            sqlCommand.Parameters.AddWithValue("@Dept", string.Join(",", selectedItems));
+                            sqlCommand.Parameters.AddWithValue("@Salary", sliderValue.Text);
+                            sqlCommand.Parameters.AddWithValue("@Date", date);
+                            sqlCommand.Parameters.AddWithValue("@Note", note_txt.Text);
+
+                            sqlCommand.ExecuteNonQuery();
+                            connection.Close();
+                            MessageBox.Show("Data added successfully", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                            Clear();
+                            Dashboard dashboard = new Dashboard();
+                            dashboard.LoadGrid();
+                            dashboard.Show();
+                            this.Close();
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                }
+            }
+            else
+            {
+                if (isValid())
+                {
+                    string date = date_combo.Text + " " + month_combo.Text + " " + year_combo.Text;
+                    var selectedItems = listOption.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => x.Content).ToList();
+                    using (connection)
+                        try
+                        {
+                            SqlCommand sqlCommand = new SqlCommand("dbo.UpdateEmployeeTable", connection);
+
+                            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                            connection.Open();
+
+                            sqlCommand.Parameters.AddWithValue("@EmpID", EmpID);
+                            sqlCommand.Parameters.AddWithValue("@Name", name_txt.Text);
+                            sqlCommand.Parameters.AddWithValue("@Profile", profileLink);
+                            sqlCommand.Parameters.AddWithValue("@Gender", genderMenu.Text);
+                            sqlCommand.Parameters.AddWithValue("@Dept", string.Join(",", selectedItems));
+                            sqlCommand.Parameters.AddWithValue("@Salary", sliderValue.Text);
+                            sqlCommand.Parameters.AddWithValue("@Date", date);
+                            sqlCommand.Parameters.AddWithValue("@Note", note_txt.Text);
+
+                            sqlCommand.ExecuteNonQuery();
+                            connection.Close();
+                            MessageBox.Show("Data updated successfully", "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                            Clear();
+                            Dashboard dashboard = new Dashboard();
+                            dashboard.LoadGrid();
+                            dashboard.Show();
+                            this.Close();
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                }
+            }
+        }
+        private void Reset_Click(object sender, RoutedEventArgs e) 
+        {
+            Clear();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e) 
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            dashboard.LoadGrid();
+            this.Close();
+        }
+
+        private void radio_Checked(object sender, RoutedEventArgs e)
+        {
+            profileLink = (string)(sender as RadioButton).Content;
+        }
+
+    }
+}
